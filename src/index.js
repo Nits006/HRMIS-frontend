a53e896ce45cc045f8fed59a462b663ee5a14739
deependra <deependra.itech@gmail.com>
2017-12-30 02:17:28 +0530

    route changes done

--- src/index.js
+++ src/index.js
@@ -7,15 +7,22 @@ import thunk from 'redux-thunk';
 import {store } from './store/store';
 import {Provider} from 'react-redux';
 import App from './App';
-import layout from './layout';
+import Layout from './layout';
 import { LoginPage } from './loginpage/LoginPage';
 import {HashRouter, Switch, Route, Link } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 
+
+const history = syncHistoryWithStore(browserHistory, store)
 ReactDOM.render(
     <Provider store={store}>
-	<HashRouter>
-	 <Route path="/" exact={true} component={LoginPage} />
+	<HashRouter history={history}>
+	<div>
+	 <Route path="/dashboard" exact={true} component={Layout}/>
+	 <Route path="/" exact={true}component={LoginPage}/>
+	 </div>
 	</HashRouter>
 	</Provider>, 
 	document.getElementById('root'));
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -4,12 +4,12 @@ import { authentication } from './authentication.reducer';
 import { registration } from './registration.reducer';
 import { users } from './users.reducer';
 import { alert } from './alert.reducer';
-
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 const rootReducer = combineReducers({
   authentication,
   registration,
   users,
-  alert
+  alert, routing: routerReducer
 });
 
 export default rootReducer;
